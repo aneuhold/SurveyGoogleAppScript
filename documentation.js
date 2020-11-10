c0597d66ac0c5be8346ef577e18ce52e9f4615e9
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-11-09 18:08:32 -0800

    Update backlog

--- documentation.js
+++ documentation.js
@@ -18,3 +18,14 @@
 *   otherAnswers: string[],
 * }
 * */
+
+/**
+ * Backlog as of 11/9/2020:
+ *
+ * - Maybe ask the group before the name and then have the select of asurite in
+ * the group section with only the group members to select from
+ * - Include a thing that says “yes” you gotta evaluate yourself
+ * - Switch the ABC grade to percentages, so let students choose ABC etc but
+ * then map it to 100, 97 etc so the mean can be calculated easily and I can
+ * use that easily in my grading sheets
+ */
