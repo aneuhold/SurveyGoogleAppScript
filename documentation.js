b16838100213afcfd667b86e8d6ff1b6d705d53b
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-11-13 10:58:09 -0800

    Update docuemntation.js

--- documentation.js
+++ documentation.js
@@ -23,13 +23,10 @@
 /**
  * Backlog as of 11/9/2020:
  *
- * - Maybe ask the group before the name and then have the select of asurite in
- * the group section with only the group members to select from
  * - Include a thing that says “yes” you gotta evaluate yourself
  * - Switch the ABC grade to percentages, so let students choose ABC etc but
  * then map it to 100, 97 etc so the mean can be calculated easily and I can
  * use that easily in my grading sheets
  * - Change the nubmering of the extra questions to be exclusive of the last
  * value. This way the user can use no extra questions
- * - Make the adding of questions to the form faster
  */
