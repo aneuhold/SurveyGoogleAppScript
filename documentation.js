9588af8e45c4f3e75057f35a76a3f5adad5cfc2d
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-11-13 15:08:23 -0800

    Update documentation

--- documentation.js
+++ documentation.js
@@ -1,10 +1,6 @@
 /**
  * Backlog as of 11/13/2020:
  *
- * - Make it so that a student's grade for themself doesn't contribute towards
- * their grade.
- * - Test to see if requiring responses to the grading questions does anything
- * strange.
  * - Change the numbering of the extra questions to be exclusive of the last
  * value. This way the user can use no extra questions
  */
